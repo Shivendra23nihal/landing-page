20f680d929bd8f31fd01cee798ee2fcc2024a4cd
Shivendra23nihal <119344916+Shivendra23nihal@users.noreply.github.com>
2024-09-10 14:49:01 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+// Toggle mobile menu
+const menuToggle = document.querySelector('.menu-toggle');
+const navLinks = document.querySelector('.nav-links');
+
+menuToggle.addEventListener('click', () => {
+    menuToggle.classList.toggle('active');
+    navLinks.classList.toggle('active');
+});
